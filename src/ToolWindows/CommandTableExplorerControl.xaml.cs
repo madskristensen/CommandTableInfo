610e1cf1e890a517b14c3ea4e36b1d58dfcba691
Mads Kristensen <post@madskristensen.net>
2018-04-19 03:17:00 -0700

    Updates

--- src/ToolWindows/CommandTableExplorerControl.xaml.cs
+++ src/ToolWindows/CommandTableExplorerControl.xaml.cs
@@ -28,24 +28,44 @@ namespace CommandTableInfo.ToolWindows
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
+            txtFilter.Clear();
+            
             details.Visibility = Visibility.Hidden;
+            groupDetails.Visibility = Visibility.Hidden;
 
             var view = (CollectionView)CollectionViewSource.GetDefaultView(list.ItemsSource);
             view.Filter = UserFilter;
             list.SelectionChanged += OnSelectionChanged;
+            CommandTreeItem.ItemSelected += CommandTreeItem_ItemSelected;
 
             _commands = _dto.CommandTable.GetCommands();
+        }
+
+        private void CommandTreeItem_ItemSelected(object sender, CommandTreeItem e)
+        {
+            txtPlacementSymbolicGuid.Text = e.Command.SymbolicItemId.SymbolicGuidName.Replace("guidSolutionExplorerMenu", "guidSHLMainMenu");
+            txtPlacementSymbolicId.Text = e.Command.SymbolicItemId.SymbolicDWordName;
+            txtPlacementGuid.Text = e.Command.ItemId.Guid.ToString();
+            txtPlacementId.Text = "0x" + e.Command.ItemId.DWord.ToString("x") + $" ({e.Command.ItemId.DWord})";
+            txtPlacementPriority.Text = "0x" + e.Command.Priority.ToString("x") + $" ({e.Command.Priority})";
+            txtPlacementType.Text = "Group";
+
+            groupDetails.Visibility = Visibility.Visible;
+
+            if (e.Command is CommandContainer menu)
+            {
+                txtPlacementType.Text = menu.Type.ToString();
+            }
         }
 
         private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var cmd = (EnvDTE.Command)list.SelectedValue;
 
-            txtName.Text = cmd.Name;
-            txtLocalizedName.Text = cmd.LocalizedName;
+            txtName.Content = cmd.Name;
             txtGuid.Text = cmd.Guid;
             txtId.Text =  "0x" + cmd.ID.ToString("x") + $" ({cmd.ID})";
-            lblBindings.Content = string.Join(Environment.NewLine, GetBindings(cmd.Bindings as object[]));
+            txtBindings.Text = string.Join(Environment.NewLine, GetBindings(cmd.Bindings as object[]));
 
             PopulateGroupsAsync(cmd).ConfigureAwait(false);
 
@@ -61,7 +81,11 @@ namespace CommandTableInfo.ToolWindows
 
                 if (command != null)
                 {
-                    groupView.ItemsSource = command.Placements;//.Select(p => new CommandItemViewModel(p));
+                    tree.ItemsSource = command.Placements.Select(p => new CommandTreeItem(p));
+                    
+                    txtPriority.Text = "0x" + command.Priority.ToString("x") + $" ({command.Priority})";
+                    txtPackage.Text = command.SourcePackageInfo.PackageName;
+                    txtAssembly.Text = command.SourcePackageInfo.Assembly;
                 }
 
                 loading.Visibility = Visibility.Collapsed;
@@ -106,6 +130,12 @@ namespace CommandTableInfo.ToolWindows
             {
                 CollectionViewSource.GetDefaultView(list.ItemsSource).Refresh();
             }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = (CheckBox)sender;
+            System.Windows.Forms.MessageBox.Show(cb.IsChecked.ToString());
         }
     }
 }
---
+++ src/ToolWindows/CommandTreeItem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using CommandTable;
+
+namespace CommandTableInfo.ToolWindows
+{
+    public class CommandTreeItem : TreeViewItem
+    {
+        public CommandTreeItem(CommandGroup item)
+        {
+            Command = item;
+            Header = item.SymbolicItemId.SymbolicDWordName;
+
+            if (item.Placements.Any())
+            {
+                var placements = new TreeViewItem { Header = "Placements" };
+                placements.ItemsSource = item.Placements.Select(p => new CommandTreeItem(p));
+
+                Items.Add(placements);
+            }
+
+            if (item.Items.Any())
+            {
+                var items = new TreeViewItem { Header = "Commands" };
+                items.ItemsSource = item.Items.Select(p => new CommandTreeItem(p));
+
+                Items.Add(items);
+            }
+        }
+
+        public CommandTreeItem(CommandContainer item)
+        {
+            Command = item;
+            Header = item.SymbolicItemId.SymbolicDWordName;
+
+            if (item.Placements.Any())
+            {
+                var placements = new TreeViewItem { Header = "Placements" };
+                placements.ItemsSource = item.Placements.Select(p => new CommandTreeItem(p));
+
+                Items.Add(placements);
+            }
+        }
+
+        public CommandTreeItem(CommandItem item)
+        {
+            Command = item;
+            Header = item.SymbolicItemId.SymbolicDWordName;
+        }
+
+        public CommandItem Command { get; set; }
+
+        public static event EventHandler<CommandTreeItem> ItemSelected;
+
+        protected override void OnSelected(RoutedEventArgs e)
+        {
+            if (IsSelected)
+            {
+                //SetResourceReference(TextElement.ForegroundProperty, EnvironmentColors.SystemHighlightTextBrushKey);
+                ItemSelected?.Invoke(this, this);
+            }
+        }
+    }
+}
